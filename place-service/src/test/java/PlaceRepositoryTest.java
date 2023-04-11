c2091eeb7a43055d2f6da530252569167b47bc2d
Clement Escoffier <clement@apache.org>
2023-04-11 09:29:31 +0200

    Add more tests for the places and player services

---
+++ place-service/src/test/java/PlaceRepositoryTest.java
@@ -0,0 +1,62 @@
+import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.geo.GeoSearchArgs;
+import io.quarkus.redis.datasource.geo.GeoUnit;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.common.mapper.TypeRef;
+import jakarta.inject.Inject;
+import org.acme.hideandseek.places.Place;
+import org.acme.hideandseek.places.PlaceRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.with;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
+class PlaceRepositoryTest {
+
+    @Autowired
+    PlaceRepository repository;
+
+    @Autowired
+    RedisDataSource redis;
+
+    @Test
+    void testAllPlaces() {
+        var places = repository.getPlaces();
+        for (Place place : places) {
+            Assertions.assertNotNull(place.coordinates());
+            Assertions.assertNotNull(place.name());
+            Assertions.assertNotNull(place.description());
+            Assertions.assertNotNull(place.picture());
+        }
+    }
+
+    @Test
+    void testSearch() {
+        var royal = repository.search("royal");
+        Assertions.assertEquals(2, royal.size());
+        for (Place place : royal) {
+            Assertions.assertNotNull(place.coordinates());
+            Assertions.assertNotNull(place.name());
+            Assertions.assertNotNull(place.description());
+            Assertions.assertNotNull(place.picture());
+        }
+
+        var brouette = repository.search("brouette");
+        Assertions.assertTrue(brouette.isEmpty());
+    }
+
+    @Test
+    void testGeoIndices() {
+        var place = repository.getPlaces().get(0);
+        var list = redis.geo(String.class).geosearch("hide-and-seek:geo", new GeoSearchArgs<String>().fromMember(place.name()).withDistance().byRadius(100, GeoUnit.KM));
+        Assertions.assertEquals(repository.getPlaces().size(), list.size());
+    }
+}
